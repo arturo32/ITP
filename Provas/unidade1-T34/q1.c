7b055f647b25f5815743fece5694262fd3d8c783
Arturo Fonseca <49870466+arturo32@users.noreply.github.com>
2019-10-02 21:42:40 -0300

    Deixando mais organizado
    O código segue a mesma lógica que anterior (estranha) mas é mais reduzido. Adicionei a função memset no vetor "apareceu" para que não se leia lixo dele.

--- Provas/unidade1-T34/q1.c
+++ Provas/unidade1-T34/q1.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 	char mensagem[5000];
 	int apareceu[27];
 	char letra;
-	int c, tam, ok = 1, i = 0;
+	int c, tam, i, ok = 1;
 
-	//Le a mensagem
-	do
-	{
-		scanf("%c", &mensagem[i]);
-		i++;
+	//Inicializa o array "apareceu" com 0's
+	memset(apareceu, 0, 27);
 
-	}while(mensagem[i - 1] != '\n');
+	//Lê a mensagem
+	scanf("%[^\n]", &mensagem[i]);
 
 	//Armazena o tamanho da mensagem
-	tam = (i - 1); 
-
+	tam = strlen(mensagem);
 
 	//Percorre o vetor mensagem e identifica as letras que apareceram
 	for (i = 0; i < tam; ++i)
@@ -25,35 +23,29 @@ int main()
 		if (mensagem[i] != ' ')
 		{
 			if (mensagem[i] >= 97) mensagem[i] -= 32;
+
 			apareceu[mensagem[i] - 65] = 1;
 		}
 			
 		
 	}
 
-	//Le a quantidade de chutes
+	//Lê a quantidade de chutes
 	scanf("%d", &c);
 
-	//Le os chutes dos minino
+	//Lê os chutes dos minino
 	for (i = 0; i < c; ++i)
 	{
 		scanf(" %c", &letra);
 		//Checa se a letra existe na mensagem
-		if (apareceu[letra - 65]) apareceu[letra - 65] = 0;
-			//Se sim, o vetor naquela posição recebe 0
+		if (!apareceu[letra - 65]) ok = 0;
+			//Se não, ok = 0 e Cabeças vão rolar
 		
 	}
-
-	//Checa se o vetor apareceu está todo zerado
-	for (i = 0; i < 27; ++i)
-	{
-		if (apareceu[i]) ok = 0;
-	}
-	
 
 	//Imprime a saída
 	if (ok) printf("Bucho Cheio!\n");
 	else printf("Cabeças vão rolar\n");
-
+	
 	return 0;
 }
